a9b23d5d2c77d4a4df5f9bd2b1f261aeca626421
Jack Herrington <jherr@pobox.com>
2021-02-02 20:47:53 -0800

    Updates

---
+++ extension/src/plugin/extension.plugin.js
@@ -0,0 +1,23 @@
+import { Button, Heading } from "@chakra-ui/react";
+
+export default {
+  "growlers/filter": {
+    function: (beverages) => {
+      return beverages[0]
+        .filter(({ producerName }) => producerName === "Boneyard")
+        .slice(0, 5);
+    },
+  },
+  "growlers/Header": {
+    function: () => (
+      <Heading as="h2" size="xl">
+        Happy Value Growlers Header
+      </Heading>
+    ),
+  },
+  "growlers/TapList": {
+    "member-function": {
+      renderAddToChart: () => <Button colorScheme="orange">Add To Cart</Button>,
+    },
+  },
+};
---
+++ growlers/src/App.js
@@ -0,0 +1,67 @@
+import { Component } from "react";
+import {
+  ChakraProvider,
+  Heading,
+  Box,
+  Text,
+  Grid,
+  Image,
+} from "@chakra-ui/react";
+
+import beverages from "./hv-taplist.json";
+
+/** @namespace growlers/Header */
+export const Header = () => (
+  <Heading as="h2" size="xl">
+    Default Header
+  </Heading>
+);
+
+/** @namespace growlers/Footer */
+export const Footer = () => (
+  <Heading as="h2" size="xl">
+    Default Footer
+  </Heading>
+);
+
+/** @namespace growlers/filter */
+export const filter = (list) => list.slice(0, 10);
+
+/** @namespace growlers/TapList */
+class TapList extends Component {
+  renderAddToChart() {
+    return null;
+  }
+
+  render() {
+    return (
+      <div>
+        {filter(beverages).map((bev, index) => (
+          <Grid templateColumns="1fr 10fr" p="2" key={index}>
+            <Image src={bev.logo} alt="Beverage logo" />
+            <Box p="2">
+              <Text size="2xl">
+                <strong>{bev.producerName}</strong>: {bev.beverageName}
+              </Text>
+              {this.renderAddToChart(bev)}
+            </Box>
+          </Grid>
+        ))}
+      </div>
+    );
+  }
+}
+
+function App() {
+  return (
+    <ChakraProvider>
+      <Box width="6xl" margin="auto">
+        <Header />
+        <TapList />
+        <Footer />
+      </Box>
+    </ChakraProvider>
+  );
+}
+
+export default App;
